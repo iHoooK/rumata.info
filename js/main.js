39205cfae55eb172db4118d627f9debf9ce1f475
iHoooK <51095455+iHoooK@users.noreply.github.com>
2021-03-15 17:14:26 +0300

    Комит 1
    Добавил файлы

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		speed: 1500,
+		fade: true,
+		cssEase: 'linear'
+	});
+});
+
+
+// $(document).ready(function(){
+// 	$('.slider').slick({
+// 		arrows:true,
+// 		dots:true,
+// 		slidesToShow:3,
+// 		autoplay:true,
+// 		speed:1000,
+// 		autoplaySpeed:800,
+// 		responsive:[
+// 			{
+// 				breakpoint: 768,
+// 				settings: {
+// 					slidesToShow:2
+// 				}
+// 			},
+// 			{
+// 				breakpoint: 550,
+// 				settings: {
+// 					slidesToShow:1
+// 				}
+// 			}
+// 		]
+// 	});
+// });
+
